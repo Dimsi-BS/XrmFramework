4ab094c184e845f18710a0890f504b400b921018
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2021-04-26 22:40:43 +0200

    Modifications

--- src/XrmFramework.Templates/templates/Plugin/initXrm.ps1
+++ src/XrmFramework.Templates/templates/Plugin/initXrm.ps1
@@ -11,3 +11,25 @@ cd ..
 dotnet sln $solutionName.Name add .\$safeprojectname$\$safeprojectname$.csproj
 
 dotnet sln $solutionName.Name add .\Utils\Deploy.$safeprojectname$\Deploy.$safeprojectname$.csproj
+
+$solutionUniqueName = Read-Host -Prompt 'Solution unique name'
+
+$configFileName = "$PSScriptRoot\..\Config\xrmFramework.config"
+
+$exists = Test-Path $configFileName -PathType leaf
+
+Write-Host "config : $configFileName ($exists)"
+
+[xml]$xmlDoc = Get-Content $configFileName
+
+Write-Host $xmlDoc.xrmFramework.projects.ChildNodes[0].name
+
+$project = $xmlDoc.CreateElement("add")
+
+$project.SetAttribute("name", "$safeprojectname$")
+$project.SetAttribute("targetSolution", $solutionUniqueName)
+$project.SetAttribute("type", "PluginsWorkflows")
+
+$xmlDoc.xrmFramework.projects.AppendChild($project)
+
+$xmlDoc.Save($configFileName)
