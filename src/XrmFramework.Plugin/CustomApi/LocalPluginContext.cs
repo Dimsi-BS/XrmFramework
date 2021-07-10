a46a4159ae6b13a95c5990ada0397f3b00627f73
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2021-07-10 14:19:58 +0200

    Moving CustomApiContext to partial CustomApi/LocalPluginContext

---
+++ src/XrmFramework.Plugin/CustomApi/LocalPluginContext.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+
+// ReSharper disable once CheckNamespace
+namespace XrmFramework
+{
+    partial class LocalPluginContext : ICustomApiContext
+    {
+               
+        public T GetArgumentValue<T>(CustomApiInArgument<T> argument)
+        {
+            if (!PluginExecutionContext.InputParameters.TryGetValue(argument.ArgumentName, out var argumentValue) || argumentValue == null)
+            {
+                return default;
+            }
+            
+            switch (argument.ArgumentType)
+            {
+
+                case CustomApiArgumentType.String:
+                    if (argument.IsSerializedArgument)
+                    {
+                        return JsonConvert.DeserializeObject<T>((string)argumentValue);
+                    }
+                    else
+                    {
+                        return (T)argumentValue;
+                    }
+                default:
+                    return (T)argumentValue;
+            }
+        }
+
+        public void SetArgumentValue<T>(CustomApiOutArgument<T> argument, T value)
+        {
+            switch (argument.ArgumentType)
+            {
+                case CustomApiArgumentType.String:
+                    if (argument.IsSerializedArgument)
+                    {
+                        PluginExecutionContext.OutputParameters[argument.ArgumentName] = JsonConvert.SerializeObject(value);
+                    }
+                    else
+                    {
+                        PluginExecutionContext.OutputParameters[argument.ArgumentName] = value;
+                    }
+                    break;
+                default:
+                    PluginExecutionContext.OutputParameters[argument.ArgumentName] = value;
+                    break;
+            }
+        }
+
+        public bool HasArgument<T>(CustomApiInArgument<T> argument)
+            => PluginExecutionContext.InputParameters.ContainsKey(argument.ArgumentName);
+
+        public bool HasArgument<T>(CustomApiOutArgument<T> argument)
+            => PluginExecutionContext.OutputParameters.ContainsKey(argument.ArgumentName);
+    }
+}
--- src/XrmFramework.Plugin/Plugin/LocalPluginContext.cs
+++ src/XrmFramework.Plugin/Plugin/LocalPluginContext.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json;
 
 namespace XrmFramework
 {
-    internal partial class LocalPluginContext : LocalContext, IPluginContext, ICustomApiContext
+    internal partial class LocalPluginContext : LocalContext, IPluginContext
     {
         public LocalPluginContext(IServiceProvider serviceProvider)
             : base(serviceProvider)
@@ -82,55 +82,6 @@ namespace XrmFramework
 
         public EntityReference ObjectRef => throw new NotImplementedException();
         
-        public T GetArgumentValue<T>(CustomApiInArgument<T> argument)
-        {
-            if (!PluginExecutionContext.InputParameters.TryGetValue(argument.ArgumentName, out var argumentValue) || argumentValue == null)
-            {
-                return default;
-            }
-            
-            switch (argument.ArgumentType)
-            {
-
-                case CustomApiArgumentType.String:
-                    if (argument.IsSerializedArgument)
-                    {
-                        return JsonConvert.DeserializeObject<T>((string)argumentValue);
-                    }
-                    else
-                    {
-                        return (T)argumentValue;
-                    }
-                default:
-                    return (T)argumentValue;
-            }
-        }
-
-        public void SetArgumentValue<T>(CustomApiOutArgument<T> argument, T value)
-        {
-            switch (argument.ArgumentType)
-            {
-                case CustomApiArgumentType.String:
-                    if (argument.IsSerializedArgument)
-                    {
-                        PluginExecutionContext.OutputParameters[argument.ArgumentName] = JsonConvert.SerializeObject(value);
-                    }
-                    else
-                    {
-                        PluginExecutionContext.OutputParameters[argument.ArgumentName] = value;
-                    }
-                    break;
-                default:
-                    PluginExecutionContext.OutputParameters[argument.ArgumentName] = value;
-                    break;
-            }
-        }
-
-        public bool HasArgument<T>(CustomApiInArgument<T> argument)
-            => PluginExecutionContext.InputParameters.ContainsKey(argument.ArgumentName);
-
-        public bool HasArgument<T>(CustomApiOutArgument<T> argument)
-            => PluginExecutionContext.OutputParameters.ContainsKey(argument.ArgumentName);
 
     }
 
