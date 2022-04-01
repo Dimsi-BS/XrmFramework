fae3e3dded5d192b42b9c6ceefd4aae15f9fa924
Pierre DLVM <p.delavillemontbazon@dimsi.fr>
2022-04-01 18:03:57 +0200

    AttributeTypeTranslator

--- src/XrmFramework.Core/AttributeTypeTranslator.cs
+++ src/XrmFramework.Core/AttributeTypeTranslator.cs
@@ -14,10 +14,33 @@ namespace XrmFramework.Core
         public static readonly Dictionary<AttributeTypeCode, string> dictionnary = new Dictionary<AttributeTypeCode, string>
         {
             { AttributeTypeCode.Boolean, "bool"},
-            { AttributeTypeCode.Decimal, "float"},
+            { AttributeTypeCode.Decimal, "decimal"},
             { AttributeTypeCode.DateTime, "DateTime"},
             { AttributeTypeCode.Double, "double"},
             { AttributeTypeCode.Integer, "int"},
+            { AttributeTypeCode.String, "string"},
+            { AttributeTypeCode.EntityName, "string"},
+            {AttributeTypeCode.BigInt,"Long" },
+            {AttributeTypeCode.Memo,"string" },
+
+            //{AttributeTypeCode.Lookup,"" },
+            //{AttributeTypeCode.ManagedProperty,"" },
+            //{AttributeTypeCode.Customer,"" },
+            //{AttributeTypeCode.PartyList,"" },
+
         };
+
+        public static string Translate(AttributeTypeCode type)
+        {
+            if (dictionnary.ContainsKey(type))
+            {
+                return dictionnary[type];
+
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
