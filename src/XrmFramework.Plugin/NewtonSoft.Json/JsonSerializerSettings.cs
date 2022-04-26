10ac5b31b278977b0f5d2f871063db1caa82742b
Aymeric SCHNEIDER-MAUNOURY <aymericsm@gmail.com>
2022-04-26 17:16:34 +0200

    [Merge] Some error occured during merging the test projects

--- src/XrmFramework.Plugin/NewtonSoft.Json/JsonSerializerSettings.cs
+++ src/XrmFramework.Plugin/NewtonSoft.Json/JsonSerializerSettings.cs
@@ -23,14 +23,15 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 #endregion
 
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Runtime.Serialization.Formatters;
-using Newtonsoft.Json.Serialization;
-using System.Runtime.Serialization;
 using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using ErrorEventArgs = Newtonsoft.Json.Serialization.ErrorEventArgs;
 
 namespace Newtonsoft.Json
 {
