1770613879157071dc364862dad2d4f381196558
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2023-05-09 15:04:11 +0200

    Correction BindingModelHelper

--- src/XrmFramework/BindingModel/BindingModelHelper.cs
+++ src/XrmFramework/BindingModel/BindingModelHelper.cs
@@ -600,7 +600,7 @@ namespace XrmFramework.BindingModel
                                     var tempLookupAttribute = entityDefinition.GetCrmLookupAttributes(crmAttribute.AttributeName).FirstOrDefault();
                                     if (tempLookupAttribute != null)
                                     {
-                                        Guid tempGuidValue = value as Guid? ?? ((Guid?)value).Value;
+                                        Guid tempGuidValue = value as Guid? ?? Guid.Empty;
 
                                         refValue = tempGuidValue == Guid.Empty ? null : new EntityReference(tempLookupAttribute.TargetEntityName, tempGuidValue);
                                     }
