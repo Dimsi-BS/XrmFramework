bd9750def7e916a1c56d4e463c504990c7e03691
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2026-04-19 10:20:15 +0200

    feat: add XrmFramework sample solution

---
+++ samples/Utils/Deploy.XrmFramework.Sample.Plugins/Program.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Christophe Gondouin (CGO Conseils). All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using XrmFramework.DeployUtils;
+
+RegistrationHelper.RegisterPluginsAndWorkflows<XrmFramework.Plugin>("XrmFramework.Sample.Plugins", false, args);
---
+++ samples/Utils/RemoteDebugger/Program.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Christophe Gondouin (CGO Conseils). All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using XrmFramework.RemoteDebugger.Client;
+using XrmFramework.RemoteDebugger.Common;
+
+var remoteDebugger = new RemoteDebugger<AzureRelayHybridConnectionMessageManager>();
+
+remoteDebugger.Start();
