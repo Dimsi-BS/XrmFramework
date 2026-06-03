5fbed6ca115e72763de0da4f20df2a31d759a903
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2026-06-03 15:46:26 +0200

    Passage des extensions sur l'ancienne syntaxe.

--- src/XrmFramework/BindingModel/IOrganizationServiceExtensions.RetrieveAll.cs
+++ src/XrmFramework/BindingModel/IOrganizationServiceExtensions.RetrieveAll.cs
@@ -32,43 +32,43 @@ public static class IOrganizationServiceExtensions_RetrieveAll
             .GetResult();
 
 #if !PLUGIN && !ON_PREMISE
-    extension(IOrganizationServiceAsync service)
+    public static async Task<IList<T>> RetrieveAllAsync<T>(this IOrganizationServiceAsync service)
+        where T : IBindingModel
     {
-        public async Task<IList<T>> RetrieveAllAsync<T>() where T : IBindingModel
-        {
-            var query = BindingModelHelper.GetRetrieveAllQuery<T>();
-            return (await service.RetrieveAllAsync(query)).ToBindingModel<T>().ToList();
-        }
-
-        public async Task<IList<T>> RetrieveAllAsync<T>(QueryExpression query, bool cleanLinks = false)
-            where T : IBindingModel
-        {
-            return (await service.RetrieveAllAsync(query, cleanLinks)).ToBindingModel<T>().ToList();
-        }
-
-        public async Task<IList<Entity>> RetrieveAllAsync(QueryExpression query, bool cleanLinks = false)
-            => await RetrieveAllInternal(query, cleanLinks, async q => await service.RetrieveMultipleAsync(q));
+        var query = BindingModelHelper.GetRetrieveAllQuery<T>();
+        return (await service.RetrieveAllAsync(query)).ToBindingModel<T>().ToList();
     }
 
-    extension(IOrganizationServiceAsync2 service)
+    public static async Task<IList<T>> RetrieveAllAsync<T>(this IOrganizationServiceAsync service,
+        QueryExpression query, bool cleanLinks = false)
+        where T : IBindingModel
     {
-        public async Task<IList<T>> RetrieveAllAsync<T>(CancellationToken cancellationToken) where T : IBindingModel
-        {
-            var query = BindingModelHelper.GetRetrieveAllQuery<T>();
-            return (await service.RetrieveAllAsync(query, cancellationToken)).ToBindingModel<T>().ToList();
-        }
-
-        public async Task<IList<T>> RetrieveAllAsync<T>(QueryExpression query, CancellationToken cancellationToken,
-            bool cleanLinks = false) where T : IBindingModel
-        {
-            return (await service.RetrieveAllAsync(query, cancellationToken, cleanLinks)).ToBindingModel<T>().ToList();
-        }
-
-        public async Task<IList<Entity>> RetrieveAllAsync(QueryExpression query, CancellationToken cancellationToken,
-            bool cleanLinks = false)
-            => await RetrieveAllInternal(query, cleanLinks,
-                async q => await service.RetrieveMultipleAsync(q, cancellationToken));
+        return (await service.RetrieveAllAsync(query, cleanLinks)).ToBindingModel<T>().ToList();
     }
+
+    public static async Task<IList<Entity>> RetrieveAllAsync(this IOrganizationServiceAsync service,
+        QueryExpression query, bool cleanLinks = false)
+        => await RetrieveAllInternal(query, cleanLinks, async q => await service.RetrieveMultipleAsync(q));
+
+    public static async Task<IList<T>> RetrieveAllAsync<T>(this IOrganizationServiceAsync2 service,
+        CancellationToken cancellationToken) where T : IBindingModel
+    {
+        var query = BindingModelHelper.GetRetrieveAllQuery<T>();
+        return (await service.RetrieveAllAsync(query, cancellationToken)).ToBindingModel<T>().ToList();
+    }
+
+    public static async Task<IList<T>> RetrieveAllAsync<T>(this IOrganizationServiceAsync2 service,
+        QueryExpression query, CancellationToken cancellationToken,
+        bool cleanLinks = false) where T : IBindingModel
+    {
+        return (await service.RetrieveAllAsync(query, cancellationToken, cleanLinks)).ToBindingModel<T>().ToList();
+    }
+
+    public static async Task<IList<Entity>> RetrieveAllAsync(this IOrganizationServiceAsync2 service,
+        QueryExpression query, CancellationToken cancellationToken,
+        bool cleanLinks = false)
+        => await RetrieveAllInternal(query, cleanLinks,
+            async q => await service.RetrieveMultipleAsync(q, cancellationToken));
 #endif
 
     private static async Task<IList<Entity>> RetrieveAllInternal(QueryExpression query, bool cleanLinks,
