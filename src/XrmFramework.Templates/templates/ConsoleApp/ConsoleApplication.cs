295dc3d6960397aff4126218c3af9ef7fd172d17
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2026-07-29 11:18:46 +0200

    refactor(templates): passer le template ConsoleApp en top-level statements
    - Program.cs : conversion en top-level statements, logique config/DI/args
      inchangee, et liberation du scope desormais assuree (using var)
    - ConsoleApplication.cs : passage en file-scoped namespace

--- src/XrmFramework.Templates/templates/ConsoleApp/ConsoleApplication.cs
+++ src/XrmFramework.Templates/templates/ConsoleApp/ConsoleApplication.cs
@@ -1,20 +1,19 @@
 ﻿using System.Threading.Tasks;
 using XrmFramework;
 
-namespace $safeprojectname$
-{
-    class ConsoleApplication
-    {
-        private readonly IService _service;
-        public ConsoleApplication(IService service)
-        {
-            _service = service;
-        }
+namespace $safeprojectname$;
 
-        public async Task RunAsync()
-        {
-            
-            
-        }
+class ConsoleApplication
+{
+    private readonly IService _service;
+
+    public ConsoleApplication(IService service)
+    {
+        _service = service;
+    }
+
+    public async Task RunAsync()
+    {
+
     }
 }
